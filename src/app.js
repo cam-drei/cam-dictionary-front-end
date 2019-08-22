a80c1b2784915f2e17ad5cea6e4923308706315c
Dung Luu <dung.luu07@gmail.com>
2019-08-22 09:53:43 +0700

    new form

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
 import './app.css';
-import TextArea from 'react-textarea-autosize';
+import TextAreaAutoSize from 'react-textarea-autosize';
+import {Container,Form,Header,Segment} from "semantic-ui-react";
 
 
-function App() {
+class App extends React.Component {
+  state = { value: "" };
+
+  render() {
+  // state = { value: "" };
+
   return (
     <div>
       <header>
@@ -22,43 +28,79 @@ function App() {
       <p class="App-title">
           Welcome to Cham Dictionary
       </p>
-
-
-      <div>
-        <Button.Group floated='left'>
-          <Button class = "navigation-active" color='blue'>Rumi</Button>
-          <Button color='blue'>Akhar Thrah</Button>
-          <Button color='blue'>Vietnamese</Button>
-        </Button.Group>
-
-        <Button color='blue'>
-          <Icon color='white' name='exchange' />
-        </Button>
-
-        <Button.Group floated='right'>
-          <Button color='blue'>Rumi</Button>
-          <Button color='blue'>Akhar Thrah</Button>
-          <Button color='blue'>Vietnamese</Button>
-        </Button.Group>
-      </div>
       
-      <br></br>
-      <br></br>
-      <div class = "text-area">
-          <TextArea style={{minHeight: 200, maxHeight: 500, width: '50%'}} defaultValue='Input your text' />
-          <TextArea style={{minHeight: 200, maxHeight: 500, width: '50%'}} defaultValue='This is the result' />
-      </div>
+      {/* <br></br> */}
+
+      <Container>
+        <Segment.Group>
+
+          <Segment>
+            <Header as="h3">
+              <Button.Group floated='left'>
+                <Button class = "navigation-active" color='blue'>Rumi</Button>
+                <Button color='blue'>Akhar Thrah</Button>
+                <Button color='blue'>Vietnamese</Button>
+              </Button.Group>
+
+              <Button color='black'>
+                <Icon color='white' name='exchange' />
+              </Button>
+            </Header>
+    
+            <Form>
+              <Form.Field
+                control={TextAreaAutoSize}
+                // label="About"
+                placeholder='Input your text'
+                onChange={e => this.setState({ value: e.target.value })}
+                // useCacheForDOMMeasurements
+                value={this.state.value}
+                style={{ boxSizing: "border-box" }}
+              />
+
+              <Form.Button
+                content="Clear"
+                onClick={() => this.setState({ value: "" })}
+                color = 'black'
+              />
+            </Form>
+          </Segment>
+
+          <Segment as={Form}>
+            <Header as="h3">
+              <Button.Group floated='left'>
+                <Button color='blue'>Rumi</Button>
+                <Button color='blue'>Akhar Thrah</Button>
+                <Button color='blue'>Vietnamese</Button>
+              </Button.Group> 
+            </Header>
+            <br></br>
+            <br></br>
+            <TextAreaAutoSize
+              placeholder='This is the result'
+              onHeightChange={(height, instance) =>
+                console.log(height, instance.rowCount)
+              }
+              useCacheForDOMMeasurements
+            />
+          </Segment>
+        </Segment.Group>
+      </Container>
+
+      {/* <div class = "text-area">
+          <TextAreaAutoSize style={{minHeight: 200, maxHeight: 500, width: '50%'}} placeholder='Input your text' />
+          <TextAreaAutoSize style={{minHeight: 200, maxHeight: 500, width: '50%'}} placeholder='This is the result' />
+      </div> */}
       
       
-      
-     
       </body>
     </div>
     
   );
 }
-
+}
 export default App;
+
 
 
 
