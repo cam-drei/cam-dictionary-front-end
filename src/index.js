5e1ee335aa305b8934c0ebc1ffe81deba0ae3df1
Dung Luu <dung.luu07@gmail.com>
2019-11-14 10:00:21 +0700

    create folder components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css'
-import App from "./app";
-
-
+import "./index.css";
+import App from "./components/app";
 
 // TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+styleLink.href =
+  "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
 document.head.appendChild(styleLink);
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
